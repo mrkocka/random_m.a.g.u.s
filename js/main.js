40e7abdb5b268cd0f61f47d6df4c454e9e976f46
Mr. Joe <radics.pepe@gmail.com>
2022-04-23 15:17:56 +0200

    2.0_1

--- js/main.js
+++ js/main.js
@@ -30,6 +30,62 @@ function myFunction() {
                 document.getElementById("js-pageNumber").innerHTML = "Bestiárium 44.olda";
         }
 
+        if ( location == 0 && D100 == 18 || D100 == 19 ){
+            document.getElementById("js-bestName").innerHTML = "Ceph";
+            document.getElementById("js-monsterStat").innerHTML = "<ul><li>KÉ: 10</li><li>TÉ: 66</li><li>VÉ: 90</li><li>CÉ: -</li><li>Sebzés:1k10/1k10</li><li>Támadás/kör: 2</li><li>ÉP: 20</li><li>FP: 100</li></ul>";
+            document.getElementById("js-pageNumber").innerHTML = "Bestiárium 46.olda";
+        }
+
+        if ( location == 0 && D100 == 20){
+            document.getElementById("js-bestName").innerHTML = "Elfo kopó";
+            document.getElementById("js-monsterStat").innerHTML = "<ul><li>KÉ: 10</li><li>TÉ: 30</li><li>VÉ: 55</li><li>CÉ: -</li><li>Sebzés:1k6</li><li>Támadás/kör: 1</li><li>ÉP: 16</li><li>FP: 34</li></ul>";
+            document.getElementById("js-pageNumber").innerHTML = "Bestiárium 73.olda";
+        }
+
+        if ( location == 0 && D100 == 21){
+            document.getElementById("js-bestName").innerHTML = "Bosszúálló";
+            document.getElementById("js-monsterStat").innerHTML = "<ul><li>KÉ: 10</li><li>TÉ: 30</li><li>VÉ: 70</li><li>CÉ: -</li><li>Sebzés:1k6/fegyver</li><li>Támadás/kör: 1</li><li>ÉP: 10</li><li>FP: -</li></ul>";
+            document.getElementById("js-pageNumber").innerHTML = "Bestiárium 78.olda";
+        }
+
+        if ( location == 0 && D100 == 22 || D100 == 23 || D100 == 24  ){
+            document.getElementById("js-bestName").innerHTML = "Gahul";
+            document.getElementById("js-monsterStat").innerHTML = "<ul><li>KÉ: 15</li><li>TÉ: 40</li><li>VÉ: 80</li><li>CÉ: -</li><li>Sebzés:1k6+2</li><li>Támadás/kör: 1</li><li>ÉP: 25</li><li>FP:-</li></ul>";
+            document.getElementById("js-pageNumber").innerHTML = "Bestiárium 83.olda";
+        }
+
+        if ( location == 0 && D100 == 25 && D100 <= 28){
+            document.getElementById("js-bestName").innerHTML = "Vérfarkas";
+            document.getElementById("js-monsterStat").innerHTML = "<ul><li>KÉ: 25</li><li>TÉ: 60</li><li>VÉ: 75</li><li>CÉ: -</li><li>Sebzés:1k10</li><li>Támadás/kör: 1</li><li>ÉP: -</li><li>FP:44</li></ul>";
+            document.getElementById("js-pageNumber").innerHTML = "Bestiárium 97.olda";
+        }
+        if ( location == 0 && D100 == 25 && D100 <= 28){
+            document.getElementById("js-bestName").innerHTML = "Zombi";
+            document.getElementById("js-monsterStat").innerHTML = "<ul><li>KÉ: 0</li><li>TÉ: 10</li><li>VÉ: 40</li><li>CÉ: -</li><li>Sebzés:1k6</li><li>Támadás/kör: 1</li><li>ÉP: 15</li><li>FP:-</li></ul>";
+            document.getElementById("js-pageNumber").innerHTML = "Bestiárium 97.olda";
+        }
+
+        if ( location == 0 && D100 == 32){
+            document.getElementById("js-bestName").innerHTML = "Feenhar";
+            document.getElementById("js-monsterStat").innerHTML = "<ul><li>KÉ: 30</li><li>TÉ: 55</li><li>VÉ: 95</li><li>CÉ: -</li><li>Sebzés:1k10+2</li><li>Támadás/kör: 1</li><li>ÉP: 20</li><li>FP:60</li></ul>";
+            document.getElementById("js-pageNumber").innerHTML = "Bestiárium 99.olda";
+        }
+
+        if ( location == 0 && D100 == 33){
+            document.getElementById("js-bestName").innerHTML = "Gider";
+            document.getElementById("js-monsterStat").innerHTML = "<ul><li>KÉ: 10</li><li>TÉ: 25</li><li>VÉ: 55</li><li>CÉ: -</li><li>Sebzés:1k6</li><li>Támadás/kör: 1</li><li>ÉP: 4</li><li>FP:12</li></ul>";
+            document.getElementById("js-pageNumber").innerHTML = "Bestiárium 103.olda";
+        }
+
+        if ( location == 0 && D100 >= 34 && D100 <= 39){
+            document.getElementById("js-bestName").innerHTML = "Goblim";
+            document.getElementById("js-monsterStat").innerHTML = "<ul><li>KÉ: 10</li><li>TÉ: 25</li><li>VÉ: 60</li><li>CÉ: -</li><li>Sebzés:fegyver szerint</li><li>Támadás/kör: 1</li><li>ÉP: 7</li><li>FP:12</li></ul>";
+            document.getElementById("js-pageNumber").innerHTML = "Bestiárium 107.olda";
+        }
+
+
+
+
 
 
         //II.Dzsungel
